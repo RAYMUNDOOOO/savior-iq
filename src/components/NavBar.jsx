7a2db52523ea6f29a9a70c961dd2eeda4f0034c7
clmntd <62601115+clmntd@users.noreply.github.com>
2024-03-27 01:12:00 +1100

    install react router

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,15 @@
 import { useState } from "react";
 import styles from "./Navbar.module.css";
 
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+  Outlet,
+  Link,
+} from "react-router-dom";
+
 function Navbar() {
   // adding the states
   const [isActive, setIsActive] = useState(false);
@@ -17,26 +26,38 @@ function Navbar() {
       <header className="App-header">
         <nav className={`${styles.navbar}`}>
           {/* logo */}
-          <a href="#home" className={`${styles.logo}`}>
+          <a href="/home" className={`${styles.logo}`}>
             The Oracle{" "}
           </a>
           <ul className={`${styles.navMenu} ${isActive ? styles.active : ""}`}>
             <li onClick={removeActive}>
-              <a href="#home" className={`${styles.navLink}`}>
+              <a href="/about" className={`${styles.navLink}`}>
                 About
               </a>
             </li>
             <li onClick={removeActive}>
-              <a href="#home" className={`${styles.navLink}`}>
+              <a href="/whiskey" className={`${styles.navLink}`}>
                 Whiskey
               </a>
             </li>
 
             <li onClick={removeActive}>
-              <a href="#home" className={`${styles.navLink}`}>
+              <a href="/contact" className={`${styles.navLink}`}>
                 Contact
               </a>
             </li>
+
+            {/* <li onClick={removeActive}>
+              <Link to="/about.jsx" className={`${styles.navLink}`}>
+                About
+              </Link>
+            </li>
+
+            <li onClick={removeActive}>
+              <a href="#home" className={`${styles.navLink}`}>
+                Test
+              </a>
+            </li> */}
           </ul>
           <div
             className={`${styles.hamburger} ${isActive ? styles.active : ""}`}
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +9,11 @@ export default defineConfig({
     modules: {
       // Enable CSS modules globally
       generateScopedName: "[name]__[local]__[hash:base64:5]",
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
     },
   },
 });
