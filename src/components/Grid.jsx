4ddf12336ae97e9f892711d06f013203614b9754
Ramon Tovar <rtovar.develops@gmail.com>
2024-04-28 11:52:18 +1000

    Tiles conditionally rendering image and linking to URL depending on passed in mode.

--- src/components/Grid.jsx
+++ src/components/Grid.jsx
@@ -9,32 +9,37 @@ import {
 } from "react-router-dom";
 
 
-export default function Grid() {
+/*
+ * The toggle switch component will pass down its state into the Grid, 
+ * which will then pass it into the tiles. The filter component also 
+ * needs to pass in which filters are currently being applied so that we 
+ * can selectively render tiles that also use that filter.
+ */
+export default function Grid({ mode, appliedFilters }) {
 
 }
 
 /*
- * Each tile will render depending on the "mode" (Whiskey or Compound) by the 
+ * Each tile will render depending on the "mode" (true = Whiskey, false = Compound) by the 
  * parent component (Grid). The table of filters that will apply to the tile
- * will also be passed in by the parent component to determine whether
- * it should render or not.
+ * will also be passed in by the parent component and used in conjunction with the
+ * appliedFilters from the Filter component to determine whether it
+ * should be rendered or not.
  */
 export function Tile({ mode, img, url, filters })  {
   const [searchFilters, setSearchFilters] = useState(filters);
 
   return (
-    <section className="tile">
-      <div>
-        {mode === "Whiskey" ? (
-          <Link to={url.whiskey}>
-            <img src={img.whiskey}></img>
-          </Link>
-        ) : (
-          <Link to={url.compound}>
-            <img src={img.compound}></img>
-          </Link>
-        )}
-      </div>
-    </section>
+    <div className="tile">
+      {mode ? (
+        <Link to={url.whiskey.url}>
+          <img src={img[0]}></img>
+        </Link>
+      ) : (
+        <Link to={url.compound.url}>
+          <img src={img[1]}></img>
+        </Link>
+      )}
+    </div>
   )
 }
